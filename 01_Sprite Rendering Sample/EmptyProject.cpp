27f85aea6b1304f71e74249b9e8f8b591e660d05
yunwo <yunwo@users.noreply.github.com>
2020-04-11 03:37:37 +0900

    Sprite Rendering Update
    .

---
+++ 01_Sprite Rendering Sample/EmptyProject.cpp
@@ -0,0 +1,157 @@
+//--------------------------------------------------------------------------------------
+// File: EmptyProject.cpp
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+#include "DXUT.h"
+#include "resource.h"
+
+LPD3DXSPRITE sprite = nullptr;
+LPDIRECT3DTEXTURE9 texture = nullptr;
+
+//--------------------------------------------------------------------------------------
+// Rejects any D3D9 devices that aren't acceptable to the app by returning false
+//--------------------------------------------------------------------------------------
+bool CALLBACK IsD3D9DeviceAcceptable( D3DCAPS9* pCaps, D3DFORMAT AdapterFormat, D3DFORMAT BackBufferFormat,
+                                      bool bWindowed, void* pUserContext )
+{
+    // Typically want to skip back buffer formats that don't support alpha blending
+    IDirect3D9* pD3D = DXUTGetD3D9Object();
+    if( FAILED( pD3D->CheckDeviceFormat( pCaps->AdapterOrdinal, pCaps->DeviceType,
+                                         AdapterFormat, D3DUSAGE_QUERY_POSTPIXELSHADER_BLENDING,
+                                         D3DRTYPE_TEXTURE, BackBufferFormat ) ) )
+        return false;
+    return true;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Before a device is created, modify the device settings as needed
+//--------------------------------------------------------------------------------------
+bool CALLBACK ModifyDeviceSettings( DXUTDeviceSettings* pDeviceSettings, void* pUserContext )
+{
+    return true;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Create any D3D9 resources that will live through a device reset (D3DPOOL_MANAGED)
+// and aren't tied to the back buffer size
+//--------------------------------------------------------------------------------------
+HRESULT CALLBACK OnD3D9CreateDevice( IDirect3DDevice9* pd3dDevice, const D3DSURFACE_DESC* pBackBufferSurfaceDesc,
+                                     void* pUserContext )
+{
+    D3DXCreateSprite(pd3dDevice, &sprite);
+    D3DXCreateTextureFromFileEx(DXUTGetD3D9Device(), L"EmptyProject.jpg", -2, -2, 1, 0, D3DFMT_A8R8G8B8,
+        D3DPOOL_MANAGED, D3DX_FILTER_NONE, D3DX_FILTER_NONE, 0, nullptr, nullptr, &texture);
+    return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Create any D3D9 resources that won't live through a device reset (D3DPOOL_DEFAULT) 
+// or that are tied to the back buffer size 
+//--------------------------------------------------------------------------------------
+HRESULT CALLBACK OnD3D9ResetDevice( IDirect3DDevice9* pd3dDevice, const D3DSURFACE_DESC* pBackBufferSurfaceDesc,
+                                    void* pUserContext )
+{
+    return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Handle updates to the scene.  This is called regardless of which D3D API is used
+//--------------------------------------------------------------------------------------
+void CALLBACK OnFrameMove( double fTime, float fElapsedTime, void* pUserContext )
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Render the scene using the D3D9 device
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D9FrameRender( IDirect3DDevice9* pd3dDevice, double fTime, float fElapsedTime, void* pUserContext )
+{
+    HRESULT hr;
+
+    // Clear the render target and the zbuffer 
+    V( pd3dDevice->Clear( 0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_ARGB( 0, 45, 50, 170 ), 1.0f, 0 ) );
+
+    // Render the scene
+    if( SUCCEEDED( pd3dDevice->BeginScene() ) )
+    {
+        sprite->Begin(D3DXSPRITE_ALPHABLEND);
+        sprite->Draw(texture, nullptr, nullptr, new D3DXVECTOR3(0, 0, 0), D3DXCOLOR(1, 1, 1, 1));
+        sprite->End();
+        V( pd3dDevice->EndScene() );
+    }
+}
+
+
+//--------------------------------------------------------------------------------------
+// Handle messages to the application 
+//--------------------------------------------------------------------------------------
+LRESULT CALLBACK MsgProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam,
+                          bool* pbNoFurtherProcessing, void* pUserContext )
+{
+    return 0;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Release D3D9 resources created in the OnD3D9ResetDevice callback 
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D9LostDevice( void* pUserContext )
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Release D3D9 resources created in the OnD3D9CreateDevice callback 
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D9DestroyDevice( void* pUserContext )
+{
+    texture->Release();
+    sprite->Release();
+}
+
+
+//--------------------------------------------------------------------------------------
+// Initialize everything and go into a render loop
+//--------------------------------------------------------------------------------------
+INT WINAPI wWinMain( HINSTANCE, HINSTANCE, LPWSTR, int )
+{
+    // Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif
+
+    // Set the callback functions
+    DXUTSetCallbackD3D9DeviceAcceptable( IsD3D9DeviceAcceptable );
+    DXUTSetCallbackD3D9DeviceCreated( OnD3D9CreateDevice );
+    DXUTSetCallbackD3D9DeviceReset( OnD3D9ResetDevice );
+    DXUTSetCallbackD3D9FrameRender( OnD3D9FrameRender );
+    DXUTSetCallbackD3D9DeviceLost( OnD3D9LostDevice );
+    DXUTSetCallbackD3D9DeviceDestroyed( OnD3D9DestroyDevice );
+    DXUTSetCallbackDeviceChanging( ModifyDeviceSettings );
+    DXUTSetCallbackMsgProc( MsgProc );
+    DXUTSetCallbackFrameMove( OnFrameMove );
+
+    // TODO: Perform any application-level initialization here
+
+    // Initialize DXUT and create the desired Win32 window and Direct3D device for the application
+    DXUTInit( true, true ); // Parse the command line and show msgboxes
+    DXUTSetHotkeyHandling( true, true, true );  // handle the default hotkeys
+    DXUTSetCursorSettings( true, true ); // Show the cursor and clip it when in full screen
+    DXUTCreateWindow( L"EmptyProject" );
+    DXUTCreateDevice( true, 640, 480 );
+
+    // Start the render loop
+    DXUTMainLoop();
+
+    // TODO: Perform any application-level cleanup here
+
+    return DXUTGetExitCode();
+}
+
+
